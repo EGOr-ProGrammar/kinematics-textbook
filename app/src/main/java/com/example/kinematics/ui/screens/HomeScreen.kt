110af32fdf54e7161bbed396340322f604efe757
Egorka <need.extra.post@gmail.com>
2025-03-21 20:41:40 +0300

    Filled first topic page of TopicScreen  with information from textbook

--- app/src/main/java/com/example/kinematics/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/kinematics/ui/screens/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.example.kinematics.ui.screens
 
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -9,11 +10,13 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,7 +27,6 @@ fun HomeScreen(
     modifier: Modifier = Modifier
 ) {
     Column(
-
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = AbsoluteAlignment.Left,
         modifier = modifier
@@ -38,17 +40,20 @@ fun HomeScreen(
                 onTopicPage()
             }
         ) {
-            Text(
-                text = "Движение",
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = modifier.padding(8.dp)
-            )
-            Text(
-                text = "Понятие движения является чрезвычайно общим и охватывает самый широкий круг явлений.",
-                fontSize = 12.sp,
-                modifier = modifier.padding(8.dp)
-            )
+            Column {
+                Text(
+                    text = "Движение",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.padding(8.dp)
+                )
+                Text(
+                    text = "Понятие движения является чрезвычайно общим и охватывает самый широкий круг явлений.",
+                    fontSize = 16.sp,
+                    modifier = modifier.padding(8.dp)
+                )
+            }
         }
     }
 }
